13d8bc26bcfbb1fa9a2dfd89d661393f0cb39858
Peter <Pete5x5@users.noreply.github.com>
2020-07-06 11:58:31 -0400

    Add support for clip logging

--- alltwitchclips-dl.py
+++ alltwitchclips-dl.py
@@ -4,27 +4,28 @@ import datetime                 # Enable date and time functionality
 import os                       # Enable filesystem functionality
 from pathlib import Path        # Enable filesystem path joining
 import logging                  # Enable logging
+# breakpoint()                  # Debugging mode
 
 configDict = dict()
 configList = [line.strip().split(' = ') for line in open('config.txt')]  # pull run parameters from config.txt
-for c1 in range(len(configList)):
+for c1 in range(len(configList)):  # for each line in config file
     try:
-        configDict[configList[c1][0]] = int(configList[c1][1])
+        configDict[configList[c1][0]] = configList[c1][1]  # try to add the config setting to the dictionary
     except (IndexError, ValueError):
-        continue
-numClips = configDict.setdefault('number-of-clips', '64')  # number of clips per page, max 100, default 64
+        continue  # if no value then skip it
+numClips = int(configDict.setdefault('number-of-clips', '64')) # number of clips per page, max 100, default 64
 if numClips > 100:
     numClips = 100
-toPag = configDict.setdefault('pagination', '1')  # whether or not you want to paginate (default 1 = yes)
-if toPag != 0 and toPag != 1:
+toPag = int(configDict.setdefault('pagination', '1'))  # whether or not you want to paginate (default 1 = yes)
+if toPag != 0 and toPag != 1:  # check to make sure the value is 0 or 1
     toPag = 1
 bID = configDict.setdefault('broadcaster-id', '84842346')  # broadcaster ID to find clips for
-errLog = configDict.setdefault('error-logging', '0')  # enable error logging?  default = 0 (no logging)
-if errLog != 0 and errLog != 1:
+errLog = int(configDict.setdefault('error-logging', '0'))  # enable error logging?  default = 0 (no logging)
+if errLog != 0 and errLog != 1:  # check to make sure the value is 0 or 1
     errLog = 0
-clipLog = configDict.setdefault('clip-logging', '1')  # enable clip logging?  default = 1 (log videos, do not attempt to download anything from a URL already in the log)
-if clipLog != 0 and clipLog != 1:
-    clipLog = 0
+clipLog = int(configDict.setdefault('clip-logging', '1'))  # enable clip logging?  default = 1 (log videos, do not attempt to download anything from a URL already in the log)
+if clipLog != 0 and clipLog != 1:  # check to make sure the value is 0 or 1
+    clipLog = 1
 
 creds = [line.strip() for line in open('creds.txt')]  # pull Twitch API credentials from creds.txt
 cID = creds[0]  # Client ID from creds.txt
@@ -60,7 +61,25 @@ while True:
     else:
         break
 
-for x2 in range(len(urls)):  # for each URL...
-    print(urls[x2])  # print the URL of the clip to be downloaded
-    ytDL = subprocess.Popen(['python', '-m', 'youtube_dl', urls[x2], '-c', '-i', '--restrict-filenames', '--download-archive', 'gordy-vids', "-o'%(id)s_[]_%(title)s.%(ext)s'"])  # subprocess to run youtube-dl with the clip URL
+if clipLog == 1:  # if clip logging enabled
+    try:
+        cLog = [line.strip() for line in open('clip-log.txt')]  # load each line of clip log file if it exists
+        print('clip log found')  # for debugging
+    except (IndexError, ValueError, FileNotFoundError):  # if file cannot be loaded or there's an error...
+        clipLogFile = open('clip-log.txt', 'w')  # make clip log file
+        cLog = []  # make cLog an empty list
+        print('clip log NOT found')  # for debugging
+    newList = [w for w in urls if w not in cLog]  # make a new list from urls NOT already in the log
+    clipLogFile = open('clip-log.txt', 'a')  # set log file to append mode
+    print('append mode set')  # for debugging
+    getURL = newList  # set the new list and the urls to download
+else:
+    getURL = urls  # if clip logging is not anables just use all urls found
+
+for x2 in range(len(getURL)):  # for each URL...
+    print(getURL[x2])  # print the URL of the clip to be downloaded
+    ytDL = subprocess.Popen(['python', '-m', 'youtube_dl', getURL[x2], '-c', '-i', '--restrict-filenames', '--download-archive', 'gordy-vids', "-o'%(id)s_[]_%(title)s.%(ext)s'"])  # subprocess to run youtube-dl with the clip URL
     ytDL.wait()  # launch the subprocess, wait for it to finish before continuing
+    if clipLog == 1:  # if clip logging enabled
+        clipLogFile.write(getURL[x2] + '\n')  # append the url to the clip log file
+        print('wrote to file ' + getURL[x2])  # for debugging
